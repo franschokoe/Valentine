3f35a8700ce8363b3ec03da23699233708009cca
Frans Chokoe <fmoyagabo@gmail.com>
2026-01-31 20:42:21 +0200

    Button pannel

--- src/componets/decisionpage.jsx
+++ src/componets/decisionpage.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import '../style/style.css'
 import fingerprint from "../assets/first/fingerprint-love.jpg"
 
-export function  Decision({name}){
+export function  Decision(){
 
     const [yes , setYes] = useState()
+
+
+    // receiverName = window.prompt("Your name: ")
 
     return (
         <div className="mycontainer">
 
-            <div className="container1 shadow-xl" >
+            <div className="container1 shadow-xl/30" >
                 <img className="fingerprint" src={fingerprint}/>
-                <div>
-                    <h1>Your Name will you be my Valentine</h1>
+                <div className="sub-info">
+                    <h1>Your Name Will you be my Valentine</h1>
+
+                </div>
+                <div className="btn-part">
+                    <button>Yes ! 🌹</button>
+                    <button>No ! 😓 </button>
                 </div>
             </div>
+
         </div>
     )
 
